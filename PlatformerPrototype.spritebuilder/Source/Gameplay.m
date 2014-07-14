fa6af148f881854e7125cfd61765cc37564aae82
Morgan Chen <morganchen@Morgans-MacBook-Air.local>
2014-07-14 12:09:54 -0700

    disabled explosion particle effects due to crash bug, added player xvel cap

--- PlatformerPrototype.spritebuilder/Source/Gameplay.m
+++ PlatformerPrototype.spritebuilder/Source/Gameplay.m
@@ -14,11 +14,12 @@
 
 static const float MOVE_SPEED_MULTIPLIER = 18;                              //scalar to multiply tilt force with
 static const float EXPLOSION_RADIUS = 100;                                  //explosion radius in points
-//static const float PROJECTILE_LIFESPAN = 20;                                //projectile lifespan in frames (60 frames/sec)
 static const float EXPLOSION_FORCE_MULTIPLIER = 150000;                     //for easy fine tuning
 static const float MIN_DISTANCE = 20;
-static const float PROJECTILE_LAUNCH_FORCE = 60;
-static const float PROJECTILE_COOLDOWN = 10;                                //in 60ths of a second
+static const float PROJECTILE_LAUNCH_FORCE = 75;
+static const float PROJECTILE_COOLDOWN = 15;                                //in 60ths of a second
+static const float PLAYER_XVEL_CAP = 100;                                   //cap on player xvelocity after which player
+                                                                            //cannot accelerate further via tilt
 
 @implementation Gameplay {
     BOOL _shooting;
@@ -30,6 +31,7 @@ static const float PROJECTILE_COOLDOWN = 10;                                //in
     CCNode *_level;
     Player *_player;
     CCNode *_contentNode;
+    CCAction *_followPlayer;
 //    CCLabelTTF *_timerLabel;                                              //broken 7/11/14
 }
 
@@ -52,9 +54,8 @@ static const float PROJECTILE_COOLDOWN = 10;                                //in
     _physicsNode.contentSize = _level.contentSize;
     _physicsNode.collisionDelegate = self;
     [_physicsNode addChild:_level];
-    CCActionFollow *follow = [CCActionFollow actionWithTarget:_player worldBoundary:_level.boundingBox];
-    [_contentNode runAction:follow];
-//    CCLOG(@"x, y = %f, %f", _level.position.x, _level.position.y);        //debug
+    _followPlayer = [CCActionFollow actionWithTarget:_player worldBoundary:_level.boundingBox];
+    [_contentNode runAction:_followPlayer];
 }
 
 -(void)shoot {
@@ -65,8 +66,7 @@ static const float PROJECTILE_COOLDOWN = 10;                                //in
         [_physicsNode addChild:projectile];
         [projectile.physicsBody applyImpulse:ccp(PROJECTILE_LAUNCH_FORCE * cos(_angleToShootAt),
                                                  PROJECTILE_LAUNCH_FORCE * sin(_angleToShootAt))];
-        [projectile.physicsBody applyTorque:(arc4random() % 360)];              //apply random spin (cosmetic function only)
-        //CCLOG(@"\nx, y = %f, %f", _player.position.x, _player.position.y);
+        [projectile.physicsBody applyTorque:(arc4random() % 360)];              //apply random spin (cosmetic only)
         _coolDown = PROJECTILE_COOLDOWN;
     }
 }
@@ -139,45 +139,28 @@ static const float PROJECTILE_COOLDOWN = 10;                                //in
         CGPoint explosionVector = ccp(explosionMagnitude * (cos(angle)),  //create vector with magnitude explosionMagnitude
                                       explosionMagnitude * (sin(angle))); //and angle angle
         [_player.physicsBody applyImpulse:explosionVector];               //push player
-//        CCLOG(@"\nx, y = %f, %f", explosionVector.x, explosionVector.y);
     }
-//    [projectile.stickyJoint invalidate];
-//    projectile.stickyJoint = nil;
-    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"Explosion"];
-    explosion.autoRemoveOnFinish = TRUE;
-    explosion.position = projectile.position;
-    [projectile.parent addChild:explosion];
+//    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"Explosion"];
+//    explosion.autoRemoveOnFinish = NO;
+//    explosion.autoRemoveOnFinish = YES;
+//    explosion.position = projectile.position;
+//    [projectile.parent addChild:explosion];
     [projectile removeFromParent];
-    
-//    for(CCNode *pushable in node.children) {                              //detonate nearby projectiles
-//        float distance = powf(powf(explosionPosition.x - pushable.position.x, 2) +
-//                              powf(explosionPosition.y - pushable.position.y, 2), 0.5);
-//        if(distance < EXPLOSION_RADIUS && ([pushable isMemberOfClass:Projectile.class] && pushable != projectile)){
-//            [self detonateProjectile:(Projectile *)pushable atPosition:pushable.position inCCNode:pushable.parent];
-//            break;
-//        }
-//    }
 }
 
 -(void)update:(CCTime)delta {
-//    for(int i = 0; i < [_physicsNode.children count]; i++){
-//        if([_physicsNode.children[i] isMemberOfClass:[Projectile class]]){
-//            Projectile *projectile = (Projectile *)_physicsNode.children[i];
-//            if(projectile.lifeSpan <= 0){
-//                [self detonateProjectile:projectile atPosition:projectile.position inCCNode:_physicsNode];
-//                continue;
-//            }
-//            projectile.lifeSpan--;
-//        }
-//    }
     CMAcceleration acceleration = _motionManager.accelerometerData.acceleration;    //move player on device tilt
-    [_player.physicsBody applyImpulse:ccp(acceleration.y * MOVE_SPEED_MULTIPLIER, 0)];
-    
-    if(_player.position.x < 0 ||                                                    //check if player exits worldbounds
-       _player.position.y < 0 ||
-       _player.position.x > _level.contentSize.width){
-        [self restartLevel];
+    float accel = acceleration.y;
+    if(fabs(_player.physicsBody.velocity.x) > PLAYER_XVEL_CAP){
+        if(_player.physicsBody.velocity.x > 0 && accel > 0){
+            accel = 0;
+        }
+        else if(_player.physicsBody.velocity.x < 0 && accel < 0){
+            accel = 0;
+        }
     }
+    [_player.physicsBody applyImpulse:ccp(accel * MOVE_SPEED_MULTIPLIER, 0)];
+    
     _timeElapsed += delta;
 //    _timerLabel.string = [self convertTimeToString];
     
@@ -187,9 +170,16 @@ static const float PROJECTILE_COOLDOWN = 10;                                //in
     else if(_shooting){
         [self shoot];
     }
+    
+    if(_player.position.x < 0 ||                                                    //check if player exits worldbounds
+       _player.position.y < 0 ||
+       _player.position.x > _level.contentSize.width){
+        [self restartLevel];
+    }
 }
 
 -(void)restartLevel {
+    [_contentNode stopAction:_followPlayer];
     [[CCDirector sharedDirector] replaceScene:[CCBReader loadAsScene:@"Gameplay"]]; //reload level upon death, keep timer time
 }
 
@@ -203,13 +193,6 @@ static const float PROJECTILE_COOLDOWN = 10;                                //in
 
 -(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair projectile:(CCNode *)projectile world:(CCNode *)world {
     Projectile *myProjectile = (Projectile *)projectile;
-//    [myProjectile.stickyJoint invalidate];
-//    myProjectile.stickyJoint = nil;
-//    myProjectile.stickyJoint = [CCPhysicsJoint connectedPivotJointWithBodyA:myProjectile.physicsBody
-//                                                                       bodyB:world.physicsBody
-//                                                                     anchorA:myProjectile.anchorPointInPoints];
-//    [myProjectile.stickyJoint tryAddToPhysicsNode:_physicsNode];
-//    return TRUE;
     [self detonateProjectile:myProjectile atPosition:myProjectile.position inCCNode:_physicsNode];
 }
 
