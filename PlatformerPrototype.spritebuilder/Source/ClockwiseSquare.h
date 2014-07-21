8cd65db3082b1458acdae2c69e4133f5d754cc01
Morgan Chen <morganchen@Morgans-MacBook-Air.local>
2014-07-21 11:11:42 -0700

    added rotating squares

---
+++ PlatformerPrototype.spritebuilder/Source/ClockwiseSquare.h
@@ -0,0 +1,14 @@
+//
+//  ClockwiseSquare.h
+//  PlatformerPrototype
+//
+//  Created by Morgan Chen on 7/21/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface ClockwiseSquare : CCSprite
+
+
+@end
---
+++ PlatformerPrototype.spritebuilder/Source/ClockwiseSquare.m
@@ -0,0 +1,31 @@
+//
+//  ClockwiseSquare.m
+//  PlatformerPrototype
+//
+//  Created by Morgan Chen on 7/21/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "ClockwiseSquare.h"
+
+//static const float ROTATION_BASE_SPEED = 0.5; //in degrees per 1/60 second
+//static const float ROTATION_SPEED_RANGE = 0.1;
+
+@implementation ClockwiseSquare {
+    float _rotationSpeed;
+}
+
+-(void)didLoadFromCCB {
+    self.rotation = arc4random() % 360;
+    self.physicsBody.collisionGroup = @"world";
+//    self.physicsBody.type = CCPhysicsBodyTypeKinematic;
+    [self runAction:[CCActionRepeatForever actionWithAction:[CCActionRotateBy actionWithDuration:1 angle:15]]];
+//    _rotationSpeed = ROTATION_BASE_SPEED + ROTATION_SPEED_RANGE * (((arc4random()%100) - 50)/100.f);
+//    CCLOG(@"%f", _rotationSpeed);
+}
+
+-(void)update:(CCTime)delta {
+    
+}
+
+@end
