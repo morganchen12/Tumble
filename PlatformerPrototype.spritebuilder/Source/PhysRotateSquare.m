983033fb61237df848551c403cae0f8d6c327392
Morgan Chen <morganchen@Morgans-MacBook-Air.local>
2014-08-11 15:02:41 -0700

    added damping to rotating world

--- PlatformerPrototype.spritebuilder/Source/PhysRotateSquare.m
+++ PlatformerPrototype.spritebuilder/Source/PhysRotateSquare.m
@@ -8,9 +8,6 @@
 
 #import "PhysRotateSquare.h"
 
-static const float ROTATION_TOLERANCE = M_PI*2;                              //max angular velocity in rad/sec before damping
-static const float DAMPING_MAGNITUDE = 0.95;
-
 @implementation PhysRotateSquare {
     CCNode *_square;
     CCNode *_pivotNode;
@@ -20,17 +17,6 @@ static const float DAMPING_MAGNITUDE = 0.95;
     _square.physicsBody.collisionGroup = @"worldBrushes";
     _square.physicsBody.collisionType = @"world";
     _pivotNode.physicsBody.collisionMask = @[];
-}
-
--(void)fixedUpdate:(CCTime)delta {
-    if(fabs(self.physicsBody.angularVelocity) > ROTATION_TOLERANCE){
-        if(self.physicsBody.angularVelocity >= 0){
-            self.physicsBody.angularVelocity = (self.physicsBody.angularVelocity - ROTATION_TOLERANCE)*DAMPING_MAGNITUDE + ROTATION_TOLERANCE;
-        }
-        else {
-            self.physicsBody.angularVelocity = (self.physicsBody.angularVelocity + ROTATION_TOLERANCE)*DAMPING_MAGNITUDE - ROTATION_TOLERANCE;
-        }
-    }
 }
 
 @end
--- PlatformerPrototype.spritebuilder/Source/Square.m
+++ PlatformerPrototype.spritebuilder/Source/Square.m
@@ -8,11 +8,25 @@
 
 #import "Square.h"
 
+static const float ROTATION_TOLERANCE = M_PI;                              //max angular velocity in rad/sec before damping
+static const float DAMPING_MAGNITUDE = 0.99;
+
 @implementation Square
 
 -(void)didLoadFromCCB {
     self.physicsBody.collisionGroup = @"worldBrushes";
     self.physicsBody.collisionType = @"world";
+}
+
+-(void)fixedUpdate:(CCTime)delta {
+    if(fabs(self.physicsBody.angularVelocity) > ROTATION_TOLERANCE){
+        if(self.physicsBody.angularVelocity >= 0){
+            self.physicsBody.angularVelocity = (self.physicsBody.angularVelocity - ROTATION_TOLERANCE)*DAMPING_MAGNITUDE + ROTATION_TOLERANCE;
+        }
+        else {
+            self.physicsBody.angularVelocity = (self.physicsBody.angularVelocity + ROTATION_TOLERANCE)*DAMPING_MAGNITUDE - ROTATION_TOLERANCE;
+        }
+    }
 }
 
 @end
